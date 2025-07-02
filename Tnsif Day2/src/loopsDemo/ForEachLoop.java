0d9878acc9671d7576cf01eab75222bb5f745048
HP <HP@LAPTOP-UTIOH6O2>
2025-07-02 23:13:06 +0530

    Day2

---
+++ Tnsif Day2/src/loopsDemo/ForEachLoop.java
@@ -0,0 +1,16 @@
+package loopsDemo;
+
+public class ForEachLoop {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {10,20,30,40,50};
+		
+		//for integer array
+		for (int i:a)
+		{
+			System.out.print(i+ " ");
+		}
+	}
+
+}
---
+++ Tnsif Day2/src/loopsDemo/WhileLoop.java
@@ -0,0 +1,17 @@
+package loopsDemo;
+
+public class WhileLoop {
+	public static void main(String[] args) {
+
+		
+		int i=99;// user value/ user input 
+		
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
