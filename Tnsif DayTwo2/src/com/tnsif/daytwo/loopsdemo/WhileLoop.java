4796e107f4256a0e7678705476715d45741ce375
HP <HP@LAPTOP-UTIOH6O2>
2025-07-04 00:07:26 +0530

    Tnsif Day2

---
+++ Tnsif DayTwo2/src/com/tnsif/daytwo/loopsdemo/WhileLoop.java
@@ -0,0 +1,17 @@
+package com.tnsif.daytwo.loopsdemo;
+
+public class WhileLoop {
+	public static void main(String[] args) {
+
+		
+		int i=99;// user value/ user input 
+		
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
