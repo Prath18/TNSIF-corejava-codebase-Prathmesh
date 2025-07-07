78571e916b2c3eba97fbdd040f7c906421f8fb43
adity <adity@Aditya>
2025-07-07 09:21:40 +0530

    day six

---
+++ DaySix/src/FinalClassDemo/FinalClassDemo.java
@@ -0,0 +1,27 @@
+package FinalClassDemo;
+
+public class FinalClassDemo {
+
+	void show ()
+	{
+		System.out.println("Final class cannot be inherited");
+	}
+}
+
+
+//class Student extends FinalClass{
+//	
+//}
+
+
+
+public class finalClassDemo {
+
+	public static void main(String[] args) {
+		
+		FinalMethodClass f=new FinalMethodClass();
+		f.show();
+
+	}
+
+}
